3268442ac7665cc38004d131ace4b9b716aa1818
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-01-03 20:57:14 +0100

    Fillit

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 10:42:09 by lsauvage          #+#    #+#             */
-/*   Updated: 2017/12/01 17:20:36 by lsauvage         ###   ########.fr       */
+/*   Updated: 2018/01/03 20:53:51 by lsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,35 @@
 
 #include "../libft/libft.h"
 
-void		free_map(t_map *map);
-void		print_map(t_map *map);
-t_map		*map_new(size_t size);
-int			place(t_etris *tetris, t_map *map, t_point *point);
-void		set_piece(t_etris *tetris, t_map *map, t_point *point, char c);
+typedef struct	s_map
+{
+	int			size;
+	char		**array;
+}				t_map;
+
+typedef struct	s_point
+{
+	int			x;
+	int			y;
+}				t_point;
+
+typedef struct	s_etris
+{
+	char		**pos;
+	int			width;
+	int			height;
+	char		value;
+}				t_etris;
+
+t_list			*read_tetri(int fd);
+void			free_map(t_map *map);
+void			print_map(t_map *map);
+t_map			*map_new(size_t size);
+int				place(t_etris *tetris, t_map *map, t_point *point);
+void			set_piece(t_etris *tetris, t_map *map, t_point *point, char c);
+t_map			*solve(t_list *list);
+t_point			*point_new(int x, int y);
+t_etris			*tetris_new(char **pos, int width, int height, char c);
+void			free_tetris(t_etris *tetris);
+t_list			*free_list(t_list *list);
 #endif
---
+++ srcs/solver.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solver.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/28 15:49:03 by lsauvage          #+#    #+#             */
+/*   Updated: 2018/01/03 20:19:59 by lsauvage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+#include "../includes/fillit.h"
+
+/*
+** Backtracking
+*/
+int		solve_map(t_map *map, t_list *list)
+{
+	t_point		*point;
+	t_etris		*tetri;
+
+	if (list == NULL)
+		return (1);
+	point->y = 0;
+	tetri = (t_etris *)(list->content);
+	while (point->y < map->size - tetri->height + 1)
+	{
+		point->x = 0;
+		while (point->x < map->size - tetri->width + 1)
+		{
+			if (place(tetri, map, point))
+			{
+				if (solve_map(map, list->next))
+					return (1);
+				else
+					set_piece(tetri, map, point_new(point->x, point->y), '.');
+			}
+			point->x++;
+		}
+		point->y++;
+	}
+	return (0);
+}
+
+/*
+** Arrondi sup de la racine de n
+*/
+
+int		high_sqrt(int n)
+{
+	int		size;
+
+	size = 2;
+	while (size * size < n)
+		size++;
+	return (size);
+}
+
+/*
+** Resolution en partant de la plus petite taille possible
+*/
+
+t_map	*solve(t_list *list)
+{
+	t_map	*map;
+	int		size;
+
+	size = high_sqrt(ft_lstcount(list) * 4);
+	map = map_new(size);
+	while (!solve_map(map, list))
+	{
+		size++;
+		free_map(map);
+		map = map_new(size);
+	}
+	return (map);
+}
